fd82d8e2bc2f0c89f21e3ba3252253cc2c3d4308
Puvendhan <puvendhan14@gmail.com>
2025-05-04 20:45:36 +0100

    Added the test scripts

---
+++ tests/integration/test_endpoints.py
@@ -0,0 +1,20 @@
+# tests/integration/test_endpoints.py
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_predict_endpoint():
+    dummy_input = {
+        "sepal_length": 6.0,
+        "sepal_width": 3.0,
+        "petal_length": 4.8,
+        "petal_width": 1.8
+    }
+    response = client.post("/predict", json=dummy_input)
+
+    assert response.status_code == 200
+    body = response.json()
+    assert "prediction" in body
+    assert "prediction_label" in body
+    assert "request_id" in body
---
+++ tests/unit/test_api.py
@@ -0,0 +1,19 @@
+# tests/unit/test_api.py
+from unittest import mock
+from app import main
+import numpy as np
+
+def test_class_names():
+    assert main.class_names == ['setosa', 'versicolor', 'virginica']
+
+@mock.patch("app.main.pipeline")
+def test_predict_logic(mock_pipeline):
+    mock_pipeline.predict.return_value = [1]
+    input_data = main.IrisInput(sepal_length=5.1, sepal_width=3.5,
+                                 petal_length=1.4, petal_width=0.2)
+
+    response = main.predict(input_data)
+
+    assert response["prediction"] == 1
+    assert response["prediction_label"] == "versicolor"
+    assert "request_id" in response
