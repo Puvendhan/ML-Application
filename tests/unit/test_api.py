13c17b0f40459f30dfb5d442825a71db0f6419d1
Puvendhan <puvendhan14@gmail.com>
2025-05-05 23:16:35 +0100

    Fix: Updated the test

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,15 +1,15 @@
-# tests/unit/test_api.py
 from unittest import mock
-from app import main
-import numpy as np
+from app import main, model
 
 def test_class_names():
     assert main.class_names == ['setosa', 'versicolor', 'virginica']
 
-@mock.patch("app.main.pipeline")
 def test_predict_logic():
-    main.app.state.pipeline = mock.Mock()
-    main.app.state.pipeline.predict.return_value = [1]
+    mock_pipeline = mock.Mock()
+    mock_pipeline.predict.return_value = [1]
+
+    # Patch model.model used inside main
+    model.model = mock_pipeline
 
     input_data = main.IrisInput(sepal_length=5.1, sepal_width=3.5,
                                  petal_length=1.4, petal_width=0.2)
